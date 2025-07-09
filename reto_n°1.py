f25c36a2115ddd1f91f2096dd1baa75976cd564f
luis muñoz <luisetiemsaez12@gmail.com>
2025-07-09 17:50:08 -0400

    eliminando zip

--- reto_n°1.py
+++
@@ -1,5 +0,0 @@
-nombre =input("ingrese su nombre ")
-edad = int(input("ingrese su edad: "))
-total_edad = (edad + 20)
-
-print(f"hola mi nombre es {nombre}, tengo {edad} años y en 20 años tendré {total_edad} años")
